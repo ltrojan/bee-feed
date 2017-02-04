468b24e3fc13871daeba347adb5b464ced4efb63
lorenzo <ltrojan@mintel.com>
2017-02-04 21:43:55 +0000

    impl a general utils.get_entries method

---
+++ bee_feed/utils.py
@@ -0,0 +1,17 @@
+from . import sql_utils
+from . import rss_utils
+from . import entries
+
+
+def get_entries(db=None, named_urls=None):
+    sql_ents = entries.Entries([])
+    rss_ents = entries.Entries([])
+    if db is not None:
+        sql_ents = entries.Entries.from_db(
+            sql_utils.get_entries(db))
+    if named_urls is not None:
+        rss_ents = entries.Entries.from_rss(
+            rss_utils.get_entries(named_urls))
+    ents = sql_ents + rss_ents
+    # sort ents???
+    return ents
