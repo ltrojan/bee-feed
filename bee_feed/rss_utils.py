44a5639113b508bf708180e88677c49f4543d9c8
lorenzo <ltrojan@mintel.com>
2017-02-04 20:57:25 +0000

    broken: mv utils to rss_utils + few mods

---
+++ bee_feed/rss_utils.py
@@ -0,0 +1,17 @@
+import feedparser
+
+
+class NamedURLs(dict):
+    def add_named_url(self, name, url):
+        self[name] = url
+
+
+def get_feed(url):
+    return feedparser.parse(url)
+
+
+def gen_entries(named_urls):
+    for ori, url in named_urls.iteritems():
+        for ent in get_feed(url)['entries']:
+            ent['ori'] = ori
+            yield ent
